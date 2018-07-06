bc6b803cbb68a4c22fee2f31d8fba4595ed9ffaa
APomelnikov <expertantony@gmail.com>
2018-07-06 23:42:02 +0300

    contact form + modal

--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
-import 'normalize.css'
-import style from  './css/style.css'
+// import 'normalize.css'
+// // import style from  './css/style.css'
+
+$(document).ready(function() {
+  var contactFormPhone = document.querySelector('#phone');
+  var modalFormPhone = document.querySelector('#phoneModal');
+  var maskOptions = {
+    mask: '+{7}(000)000-00-00',
+    lazy: true
+  };
+  var mask = new IMask(contactFormPhone, maskOptions);
+  var maskModal = new IMask(modalFormPhone, maskOptions);
+  console.log(mask, maskModal)
+
+  $("#modal").iziModal({
+    overlayColor: 'rgba(0, 0, 0, 0.7)',
+  });
+
+  $(document).on('click', '.trigger', function (event) {
+    event.preventDefault();
+    console.log('open')
+    $('#modal').iziModal('open');
+  });
+
+});
