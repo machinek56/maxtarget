e0bd2f17e7e31be842f3575d27f3b3e780b7d919
Vadim Gnatenko <vadgnatenko1@gmail.com>
2018-07-13 19:17:01 +0300

    Modal box add

--- js/custom.js
+++ js/custom.js
@@ -321,6 +321,17 @@
         mobileNav();
         commingsoonHeight();
     });
+ /*-----------------------------------
+    IziModal
+    -----------------------------------*/
+        $("#modal").iziModal();
+        $(document).on('click', '.modal_open', function (event) {
+            event.preventDefault();
+            // $('#modal').iziModal('setZindex', 99999);
+            // $('#modal').iziModal('open', { zindex: 99999 });
+            $('#modal').iziModal('open');
+        });
+
 
 })(jQuery);
 
